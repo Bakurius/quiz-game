c9f438e62616060f9bbe6e7cb471ee0f0c3edf09
Shota <shotabakuridze77@gmail.com>
2025-03-23 16:51:18 +0400

    Updated server.js and User.js to use bcryptjs

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs"); // Changed to bcryptjs
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true }, // Add this
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   isAdmin: { type: Boolean, default: false },
   totalScore: { type: Number, default: 0 },
 });
 
+// Hash password before saving
 userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
